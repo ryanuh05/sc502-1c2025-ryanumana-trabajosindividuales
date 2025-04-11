bf33d34a53d2bf5546eec19ee419c0bc582f382d
Ryan Umaña <ryanuh05@gmail.com>
2025-04-10 20:24:17 -0600

    arreglo en db.php
    gracias profe

--- practica-programada-4/backend/db.php
+++ practica-programada-4/backend/db.php
@@ -8,7 +8,7 @@ try{
  
     $pdo = new PDO("mysql:host=$host;dbname=$dbname",$user,$password);
     $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    echo "Conexion exitosa ". PHP_EOL;
+    
     
 }catch(PDOException $e){
     die("Error de conexion: " . $e -> getMessage());
